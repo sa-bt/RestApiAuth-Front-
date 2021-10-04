e46f9b3837f654f628c6b4081221b2f51ecec674
sa-bt <bakhshian2020@gmail.com>
2021-10-04 22:29:03 +0330

    implement header

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Nuxt/>
+    </div>
+</template>
+
+<script>
+    import Header from "../components/Header";
+    export default {
+        name: "default",
+        components: {Header}
+    }
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <Tutorial/>
+    <div class="bg-cover min-h-screen " >
+        <img width="100%" height="100%" src="../assets/images/10.jpg">
+
+    </div>
 </template>
 
 <script>
